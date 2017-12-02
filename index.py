404d9ac88add670b8be5c09fb3ff4174dc750432
thibaut-weissgerber <thibaut.weissgerber@gmail.com>
2017-12-02 22:53:39 +0100

    Added static files, main page layout, API endpoint
    - Added Bootstrap JS / CSS / Fonts
    - Main Page Layout (ugly)
    - API Endpoint to toggle recording via POST requests

--- index.py
+++ index.py
@@ -1,15 +1,37 @@
 from flask import Flask
 from flask import render_template
+from flask import jsonify
+
 import subprocess
 import credentials
 import bashCommands
 
+testVar = bashCommands.GetCommand(1)
 app = Flask(__name__)
 
-testVar = bashCommands.GetCommand(1)
-
-@app.route("/")
+@app.route('/', methods=['GET'])
 def hello():
-    return render_template('index.html', title=testVar)
+    return render_template('index.html', title='bb')
 
-app.run(host='0.0.0.0')
+@app.route('/<cameraId>/<isRecording>', methods=['POST'])
+def toggle_camera_recording(cameraId, isRecording):
+    try:
+        cameraId = int(cameraId[-1])
+        isRecording = (isRecording == 'true')
+        body = jsonify({'id': cameraId, 'isRecording': isRecording})
+        response = body
+        response.status_code = 200
+
+        if isRecording == True:
+            return response
+        elif isRecording == False:
+            return response
+        else:
+            raise Exception('Parsing error')
+    except Exception:
+        response.status_code = 500
+
+        return response
+
+
+#app.run(host= '0.0.0.0')
