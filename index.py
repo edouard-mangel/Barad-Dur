d5b9516f9b389c1b5296b171f06d42d4c9f4d4ae
thibaut-weissgerber <thibaut.weissgerber@gmail.com>
2017-12-02 23:05:30 +0100

    Removed testVar - Added Command Output in body

--- index.py
+++ index.py
@@ -8,19 +8,16 @@ import bashCommands
 
 app = Flask(__name__)
 
-testVar = bashCommands.GetPlayCommand("FileName.avi")
-
-
 @app.route('/', methods=['GET'])
 def hello():
-    return render_template('index.html', title=testVar)
+    return render_template('index.html')
 
 @app.route('/<cameraId>/<isRecording>', methods=['POST'])
 def toggle_camera_recording(cameraId, isRecording):
     try:
         cameraId = int(cameraId[-1])
         isRecording = (isRecording == 'true')
-        body = jsonify({'id': cameraId, 'isRecording': isRecording})
+        body = jsonify({'id': cameraId, 'isRecording': isRecording, 'command': bashCommands.GetRecordCommand(cameraId)})
         response = body
         response.status_code = 200
 
